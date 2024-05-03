6380b2a2a52124ba765f2e9271e2303c5fb5c93f
Res4ad0 <rresad019@gmail.com>
2024-05-03 10:24:53 +0400

    first commit

---
+++ python.py
@@ -0,0 +1,36 @@
+# task 1
+
+# numbers = []
+
+# for i in range(5):
+#     number=int(input("Enter number:  "))
+#     numbers.append(number)
+
+# numbers.sort()
+# print(numbers)
+
+
+
+# Task 2
+
+# text = input("Metin yaz:  ")
+
+# sirali_text = ''.join(sorted(text.replace(' ',' ')))
+
+# print(sirali_text)
+
+
+
+# Task 3
+
+
+# n = 13
+# yoxlama = 0
+
+# while True:
+#     texmin = int(input("Texmin et:  "))
+#     if texmin != n:
+#         yoxlama += 1
+#     else:
+#         print(f"Sen {yoxlama} cehdde {n} reqemini tapdin")
+#         break
